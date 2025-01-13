cb6f8f3fd88cc8275cdce6073559e0c4037bf45b
aishwarya rana <ashrana1993@gmail.com>
2025-01-12 23:19:08 -0500

    added meals details page

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -1,12 +1,11 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import classes from './nav-link.module.css';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css";
 
 export default function NavLink({ href, children }) {
   const path = usePathname();
-
   return (
     <Link
       href={href}
